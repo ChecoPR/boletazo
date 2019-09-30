718a2e1111d06239ba30393057d435eb9b9f1dba
CrostiPerez <ropealse@gmail.com>
2019-09-30 17:43:22 -0500

    Nueva base de datos

--- boletazo.sql
+++ boletazo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-09-2019 a las 02:57:48
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.2.15
+-- Tiempo de generación: 01-10-2019 a las 00:37:49
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,7 +32,8 @@ CREATE TABLE `apartados` (
   `idApartado` int(11) NOT NULL,
   `idUsuario` int(11) NOT NULL,
   `idEvento` int(11) NOT NULL,
-  `reservado` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `pagado` double NOT NULL,
+  `tiempo` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -43,39 +44,8 @@ CREATE TABLE `apartados` (
 
 CREATE TABLE `asientos` (
   `idAsiento` int(11) NOT NULL,
-  `estado` tinyint(1) NOT NULL,
   `idZona` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `asientos`
---
-
-INSERT INTO `asientos` (`idAsiento`, `estado`, `idZona`) VALUES
-(1, 1, 1),
-(2, 0, 2),
-(3, 1, 3);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `boletos`
---
-
-CREATE TABLE `boletos` (
-  `idBoleto` int(11) NOT NULL,
-  `idApartado` int(11) DEFAULT NULL,
-  `idZona` int(11) NOT NULL,
-  `idAsiento` int(11) NOT NULL,
-  `idComprador` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `boletos`
---
-
-INSERT INTO `boletos` (`idBoleto`, `idApartado`, `idZona`, `idAsiento`, `idComprador`) VALUES
-(1, NULL, 3, 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -84,21 +54,44 @@ INSERT INTO `boletos` (`idBoleto`, `idApartado`, `idZona`, `idAsiento`, `idCompr
 --
 
 CREATE TABLE `eventos` (
-  `idEvento` int(100) NOT NULL,
+  `idEvento` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `hora` time NOT NULL,
-  `fecha` date NOT NULL,
-  `idLugar` int(11) NOT NULL
+  `lugar` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `eventos`
 --
 
-INSERT INTO `eventos` (`idEvento`, `nombre`, `hora`, `fecha`, `idLugar`) VALUES
-(1, 'La arrolladora', '12:00:00', '2019-09-19', 1),
-(2, 'Twenty One Pilots', '11:20:00', '2020-03-27', 2),
-(3, 'El Recodo', '15:30:00', '2019-11-20', 1);
+INSERT INTO `eventos` (`idEvento`, `nombre`, `lugar`, `hora`) VALUES
+(1, 'Daft Punk', 'Estadio Azteca', '22:00:00'),
+(2, 'José José', 'Estadio La Corregidora', '17:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventosasientos`
+--
+
+CREATE TABLE `eventosasientos` (
+  `idEvento` int(11) NOT NULL,
+  `idAsiento` int(11) NOT NULL,
+  `idApartado` int(11) NOT NULL,
+  `idZona` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventoszonas`
+--
+
+CREATE TABLE `eventoszonas` (
+  `idEvento` int(11) NOT NULL,
+  `idZona` int(11) NOT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -108,19 +101,9 @@ INSERT INTO `eventos` (`idEvento`, `nombre`, `hora`, `fecha`, `idLugar`) VALUES
 
 CREATE TABLE `lugar` (
   `idLugar` int(11) NOT NULL,
-  `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `idEvento` int(100) NOT NULL,
-  `estado` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `idEvento` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `lugar`
---
-
-INSERT INTO `lugar` (`idLugar`, `nombre`, `idEvento`, `estado`) VALUES
-(1, 'La Corregidora', 1, 'Querétaro'),
-(3, 'La Corregidora', 2, 'Querétaro'),
-(4, 'Auditorio Josefa', 3, 'Querétaro');
 
 -- --------------------------------------------------------
 
@@ -131,7 +114,7 @@ INSERT INTO `lugar` (`idLugar`, `nombre`, `idEvento`, `estado`) VALUES
 CREATE TABLE `metodospago` (
   `idMetodoPago` int(11) NOT NULL,
   `idUsuario` int(11) NOT NULL,
-  `numeroTarjeta` int(16) NOT NULL,
+  `numeroTarjeta` varchar(16) COLLATE utf8_spanish_ci NOT NULL,
   `domicilio` varchar(100) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
@@ -145,7 +128,7 @@ CREATE TABLE `usuarios` (
   `idUsuario` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `direccion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `telefono` int(10) NOT NULL
+  `telefono` varchar(12) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -156,18 +139,8 @@ CREATE TABLE `usuarios` (
 
 CREATE TABLE `zona` (
   `idZona` int(11) NOT NULL,
-  `precio` double NOT NULL,
   `idLugar` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `zona`
---
-
-INSERT INTO `zona` (`idZona`, `precio`, `idLugar`) VALUES
-(1, 200, 1),
-(2, 100, 4),
-(3, 700, 3);
 
 --
 -- Índices para tablas volcadas
@@ -178,8 +151,8 @@ INSERT INTO `zona` (`idZona`, `precio`, `idLugar`) VALUES
 --
 ALTER TABLE `apartados`
   ADD PRIMARY KEY (`idApartado`),
-  ADD UNIQUE KEY `idUsuario` (`idUsuario`),
-  ADD UNIQUE KEY `idEvento` (`idEvento`);
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idEvento` (`idEvento`);
 
 --
 -- Indices de la tabla `asientos`
@@ -189,33 +162,40 @@ ALTER TABLE `asientos`
   ADD KEY `idZona` (`idZona`);
 
 --
--- Indices de la tabla `boletos`
---
-ALTER TABLE `boletos`
-  ADD PRIMARY KEY (`idBoleto`),
-  ADD UNIQUE KEY `idAsiento` (`idAsiento`),
-  ADD UNIQUE KEY `idZona` (`idZona`),
-  ADD UNIQUE KEY `idApartado` (`idApartado`);
-
---
 -- Indices de la tabla `eventos`
 --
 ALTER TABLE `eventos`
   ADD PRIMARY KEY (`idEvento`);
 
 --
+-- Indices de la tabla `eventosasientos`
+--
+ALTER TABLE `eventosasientos`
+  ADD PRIMARY KEY (`idEvento`,`idAsiento`,`idZona`),
+  ADD KEY `idApartado` (`idApartado`),
+  ADD KEY `idAsiento` (`idAsiento`),
+  ADD KEY `idZona` (`idZona`);
+
+--
+-- Indices de la tabla `eventoszonas`
+--
+ALTER TABLE `eventoszonas`
+  ADD PRIMARY KEY (`idEvento`,`idZona`),
+  ADD KEY `idZona` (`idZona`);
+
+--
 -- Indices de la tabla `lugar`
 --
 ALTER TABLE `lugar`
   ADD PRIMARY KEY (`idLugar`),
-  ADD KEY `idEvento` (`idEvento`) USING BTREE;
+  ADD KEY `idEvento` (`idEvento`);
 
 --
 -- Indices de la tabla `metodospago`
 --
 ALTER TABLE `metodospago`
   ADD PRIMARY KEY (`idMetodoPago`),
-  ADD UNIQUE KEY `idUsuario` (`idUsuario`);
+  ADD KEY `idUsuario` (`idUsuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -228,7 +208,7 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `zona`
   ADD PRIMARY KEY (`idZona`),
-  ADD UNIQUE KEY `idLugar` (`idLugar`);
+  ADD KEY `idLugar` (`idLugar`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -244,43 +224,19 @@ ALTER TABLE `apartados`
 -- AUTO_INCREMENT de la tabla `asientos`
 --
 ALTER TABLE `asientos`
-  MODIFY `idAsiento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `boletos`
---
-ALTER TABLE `boletos`
-  MODIFY `idBoleto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idAsiento` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `idEvento` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `lugar`
---
-ALTER TABLE `lugar`
-  MODIFY `idLugar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `metodospago`
---
-ALTER TABLE `metodospago`
-  MODIFY `idMetodoPago` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `zona`
 --
 ALTER TABLE `zona`
-  MODIFY `idZona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idZona` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -290,40 +246,48 @@ ALTER TABLE `zona`
 -- Filtros para la tabla `apartados`
 --
 ALTER TABLE `apartados`
-  ADD CONSTRAINT `Apartados_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
-  ADD CONSTRAINT `Apartados_ibfk_2` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`);
+  ADD CONSTRAINT `apartados_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  ADD CONSTRAINT `apartados_ibfk_2` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`);
 
 --
 -- Filtros para la tabla `asientos`
 --
 ALTER TABLE `asientos`
-  ADD CONSTRAINT `Asientos_ibfk_1` FOREIGN KEY (`idZona`) REFERENCES `zona` (`idZona`);
+  ADD CONSTRAINT `asientos_ibfk_1` FOREIGN KEY (`idZona`) REFERENCES `zona` (`idZona`);
 
 --
--- Filtros para la tabla `boletos`
+-- Filtros para la tabla `eventosasientos`
 --
-ALTER TABLE `boletos`
-  ADD CONSTRAINT `Boletos_ibfk_2` FOREIGN KEY (`idApartado`) REFERENCES `apartados` (`idApartado`),
-  ADD CONSTRAINT `Boletos_ibfk_3` FOREIGN KEY (`idAsiento`) REFERENCES `asientos` (`idAsiento`),
-  ADD CONSTRAINT `Boletos_ibfk_4` FOREIGN KEY (`idZona`) REFERENCES `zona` (`idZona`);
+ALTER TABLE `eventosasientos`
+  ADD CONSTRAINT `eventosasientos_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`),
+  ADD CONSTRAINT `eventosasientos_ibfk_2` FOREIGN KEY (`idAsiento`) REFERENCES `asientos` (`idAsiento`),
+  ADD CONSTRAINT `eventosasientos_ibfk_3` FOREIGN KEY (`idApartado`) REFERENCES `apartados` (`idApartado`),
+  ADD CONSTRAINT `eventosasientos_ibfk_4` FOREIGN KEY (`idZona`) REFERENCES `zona` (`idZona`);
+
+--
+-- Filtros para la tabla `eventoszonas`
+--
+ALTER TABLE `eventoszonas`
+  ADD CONSTRAINT `eventoszonas_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`),
+  ADD CONSTRAINT `eventoszonas_ibfk_2` FOREIGN KEY (`idZona`) REFERENCES `zona` (`idZona`);
 
 --
 -- Filtros para la tabla `lugar`
 --
 ALTER TABLE `lugar`
-  ADD CONSTRAINT `idEvento` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`);
+  ADD CONSTRAINT `lugar_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `eventos` (`idEvento`);
 
 --
 -- Filtros para la tabla `metodospago`
 --
 ALTER TABLE `metodospago`
-  ADD CONSTRAINT `MetodosPago_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`);
+  ADD CONSTRAINT `metodospago_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`);
 
 --
 -- Filtros para la tabla `zona`
 --
 ALTER TABLE `zona`
-  ADD CONSTRAINT `Zona_ibfk_1` FOREIGN KEY (`idLugar`) REFERENCES `lugar` (`idLugar`);
+  ADD CONSTRAINT `zona_ibfk_1` FOREIGN KEY (`idLugar`) REFERENCES `lugar` (`idLugar`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
